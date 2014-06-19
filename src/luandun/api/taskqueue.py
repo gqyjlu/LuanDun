44c8320a1b2d9be8ce04e62fc2ba26224a4c652a
Prstcsnpr <prstcsnpr@PrstcsnprdeMacBook-Pro.local>
2014-06-19 10:34:19 +0800

    增加consumer

--- src/luandun/api/taskqueue.py
+++ src/luandun/api/taskqueue.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 
+
 '''
 Created on 2014年6月11日
 
 @author: prstcsnpr
 '''
 
+
 import json
 import logging
+import random
 import string
 import threading
 
@@ -17,6 +20,10 @@ import beanstalkc
 producer_manager = None
 producer_manager_lock = threading.Lock
 
+worker_manager = None
+worker_manager_lock = threading.Lock
+
+
 def add(url, queue_name, method, params):
     body = {}
     body["url"] = url
@@ -24,18 +31,75 @@ def add(url, queue_name, method, params):
     body["params"] = params
     get_producer_manager().put(queue_name, json.dumps(body))
     
+    
+def get_worker_manager():
+    global worker_manager
+    if worker_manager is None:
+        with worker_manager_lock:
+            if worker_manager is None:
+                worker_manager = WorkerManager()
+    return worker_manager
+    
 
 def get_producer_manager():
+    global producer_manager
     if producer_manager is None:
         with producer_manager_lock:
             if producer_manager is None:
-                producer_manager = ProducerManager()
+                producer_manager = ProducerManager(beanstalkc.DEFAULT_HOST + ":" + str(beanstalkc.DEFAULT_PORT))
     return producer_manager
+
+
+class Job(object):
+    
+    def __init__(self, job):
+        self.job = job
+        
+    def delete(self):
+        self.job.delete()
+        
+    def release(self):
+        self.job.release()
+        
+        
+class Worker(object):
+    
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.beanstalk = beanstalkc.Connection(host, port)
+            
+    def reserve(self, timeout):
+        return Job(self.beanstalk.reserve(timeout))
+    
+    def update_tubes(self, tubes):
+        if tubes is None:
+            tubes = self.beanstalk.tubes()
+        for tube in tubes:
+            self.beanstalk.watch(tube)
+        
+
+class WorkerManager(object):
+    
+    def __init__(self):
+        self.workers = []
+    
+    def initialize(self, addresses):
+        for address in addresses.split(","):
+            fields = address.split(":")
+            self.workers.append(Worker(fields[0], string.atoi(fields[1])))
+            
+    def update_tubes(self, tubes=None):
+        for worker in self.workers:
+            worker.update_tubes(tubes)
+            
+    def reserve(self, timeout=None):
+        return random.choice(self.workers).reserve(timeout)
 
     
 class Producer(object):
     
-    def __init__(self, host, port, tube="default"):
+    def __init__(self, host, port, tube):
         self.host = host
         self.port = port
         self.tube = tube
@@ -51,12 +115,13 @@ class Producer(object):
         
 class ProducerGroup(object):
     
-    def __init__(self, sockets):
+    def __init__(self, tube, sockets):
         self.index = -1
+        self.tube = tube
         self.producers = []
         self.lock = threading.Lock()
         for socket in sockets:
-            self.producers.append(Producer(socket[0], socket[1]))
+            self.producers.append(Producer(socket[0], socket[1], tube))
         
     def __put(self, body):
         with self.lock:
