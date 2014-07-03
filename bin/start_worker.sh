b5e982e8a5cd1fabcf2f1c67c2cb0f9e9c26a513
prstcsnpr <prstcsnpr@10.2.201.237>
2014-07-03 11:55:21 +0800

    生成目录

--- bin/start_worker.sh
+++ bin/start_worker.sh
@@ -2,4 +2,6 @@
 
 . env.sh
 
-cd $LUANDUN_HOME/src/luandun/tools/ && python worker.py -t magicformula
+mkdir -p $LUANDUN_HOME/log/worker
+
+cd $LUANDUN_HOME/src/luandun/tools/ && python worker.py -t magicformula > $LUANDUN_HOME/log/worker/stdout.log 2> $LUANDUN_HOME/log/worker/stderr.log &
