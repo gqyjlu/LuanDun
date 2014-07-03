abc595eebfb84560ee60fe5f4f1d02707501978f
prstcsnpr <prstcsnpr@10.2.201.237>
2014-07-03 14:38:48 +0800

    定制crontab任务

---
+++ bin/crontab.sh
@@ -0,0 +1,3 @@
+20 3 * * 1,2,3,4,5 source ~/.bash_profile && wget http://zhengqianhua.mouwuming.com/magicformula/updatestockinfo
+30 3 * * 1,2,3,4,5 source ~/.bash_profile && wget http://zhengqianhua.mouwuming.com/magicformula/updatemagicformula
+* * */1 * * source ~/.bash_profile && wget http://zhengqianhua.mouwuming.com/magicformula/updategdp
