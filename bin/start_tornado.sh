a2435d26cfd183b6158508105cf9b5ec33188d0f
prstcsnpr <prstcsnpr@10.2.201.237>
2014-07-03 11:59:44 +0800

    改正目录

--- bin/start_tornado.sh
+++ bin/start_tornado.sh
@@ -4,4 +4,4 @@
 
 mkdir -p $LUANDUN_HOME/log/tornado
 
-cd $LUANDUN_HOME/src/luandun/tools/ && python main.py > $LUANDUN_HOME/log/tornado/stdout.log 2> $LUANDUN_HOME/log/tornado/stderr.log &
+cd $LUANDUN_HOME/src/luandun/business/magicformula/ && python main.py > $LUANDUN_HOME/log/tornado/stdout.log 2> $LUANDUN_HOME/log/tornado/stderr.log &
