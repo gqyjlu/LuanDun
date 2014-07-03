bfb62e64a69dd780453f075f230e54246a70ba20
prstcsnpr <prstcsnpr@10.2.201.237>
2014-07-03 11:53:02 +0800

    按需生成目录

--- bin/start_beanstalkd.sh
+++ bin/start_beanstalkd.sh
@@ -2,4 +2,7 @@
 
 . env.sh
 
+mkdir -p $LUANDUN_HOME/data/beanstalkd
+mkdir -p $LUANDUN_HOME/log/beanstalkd
+
 nohup beanstalkd -l 127.0.0.1 -p 11300 -b $LUANDUN_HOME/data/beanstalkd -f0 > $LUANDUN_HOME/log/beanstalkd/beanstalkd.out 2> $LUANDUN_HOME/log/beanstalkd/beanstalkd.err &
