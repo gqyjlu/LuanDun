022a7412bb0495f6a856781aaf6db648f0b45e45
prstcsnpr <prstcsnpr@10.2.201.237>
2014-07-03 14:46:27 +0800

    改域名

--- src/luandun/business/magicformula/constant.py
+++ src/luandun/business/magicformula/constant.py
@@ -9,6 +9,6 @@ Created on 2014年7月2日
 
 
 #ToDo 改成配置
-#URL_PREFIX="http://zhengqianhua.mouwuming.com"
-URL_PREFIX="http://127.0.0.1:8888"
+URL_PREFIX="http://magicformula.mouwuming.com"
+#URL_PREFIX="http://127.0.0.1:8888"
 KEYSPACE="magicformula"
