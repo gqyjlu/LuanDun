c4c5329b6174808e31a9d10582ec625d838c6883
prstcsnpr <prstcsnpr@10.2.201.237>
2014-07-04 10:51:02 +0800

    修改

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 LuanDun
 =======
 Dependency:
-python 2.7.5+
-beanstalkc 0.4.0+
-cqlengine 0.15.4+
-tornado 3.2.1+
-gevent 1.0.1+
-eventlet 0.14.0+
+	python 2.7.5+
+	beanstalkc 0.4.0+
+	cqlengine 0.15.4+
+	tornado 3.2.1+
+	gevent 1.0.1+
+	eventlet 0.14.0+
+	PyYAML 3.11+
+	beanstalkd 0.15.4+
+	greenlet 0.4.2+
--- bin/crontab.sh
+++ bin/crontab.sh
@@ -1,3 +1,3 @@
 20 3 * * 1,2,3,4,5 source ~/.bash_profile && wget http://magicformula.mouwuming.com/magicformula/updatestockinfo
 30 3 * * 1,2,3,4,5 source ~/.bash_profile && wget http://magicformula.mouwuming.com/magicformula/updatemagicformula
-* * */1 * * source ~/.bash_profile && wget http://magicformula.mouwuming.com/magicformula/updategdp
+0 0 */1 * * source ~/.bash_profile && wget http://magicformula.mouwuming.com/magicformula/updategdp
