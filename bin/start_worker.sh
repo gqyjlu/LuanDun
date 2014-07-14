7446507a7410211e8d4569e61292e7028444240b
Prstcsnpr <prstcsnpr@PrstcsnprdeMacBook-Pro.local>
2014-07-14 15:23:32 +0800

    修改log

--- bin/start_worker.sh
+++ bin/start_worker.sh
@@ -4,4 +4,4 @@
 
 mkdir -p $LUANDUN_HOME/log/worker
 
-cd $LUANDUN_HOME/src/luandun/tools/ && python worker.py -t magicformula 2>&1 | cronolog $LUANDUN_HOME/log/worker/worker.log &
+cd $LUANDUN_HOME/src/luandun/tools/ && python worker.py -t magicformula 2>&1 | cronolog $LUANDUN_HOME/log/worker/worker.%Y%m%d.log &
