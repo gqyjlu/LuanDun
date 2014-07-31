e3fc39456a6cb9517e1e99e58884087537b6913e
prstcsnpr <prstcsnpr@10.2.202.25>
2014-07-31 11:41:30 +0800

    更新graham

--- bin/crontab.sh
+++ bin/crontab.sh
@@ -1,3 +1,4 @@
 20 15 * * 1,2,3,4,5 source ~/.bash_profile && curl -d "" http://magicformula.mouwuming.com/magicformula/updatestockinfo > /dev/null 2>&1
 30 15 * * 1,2,3,4,5 source ~/.bash_profile && curl -d "" http://magicformula.mouwuming.com/magicformula/updatemagicformula > /dev/null 2>&1
+30 15 * * 1,2,3,4,5 source ~/.bash_profile && curl -d "" http://magicformula.mouwuming.com/magicformula/updategrahamformula > /dev/null 2>&1
 0 0 */1 * * source ~/.bash_profile && curl -d "" http://magicformula.mouwuming.com/magicformula/updategdp > /dev/null 2>&1
