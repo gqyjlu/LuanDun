ffd1a0e0846109f60e2a0895a03cb7f454e6f9bf
prstcsnpr <prstcsnpr@10.2.202.25>
2014-08-12 10:46:05 +0800

    判断端口

--- bin/start_mongodb.sh
+++ bin/start_mongodb.sh
@@ -2,4 +2,8 @@
 
 . env.sh
 
-mongod --dbpath $LUANDUN_HOME/data/mongodb 2>&1 | cronolog $LUANDUN_HOME/log/mongodb/mongodb.%Y%m%d.log &
+if [ "x$MONGO_PORT" = "x" ]; then
+	MONGO_PORT = 27017
+fi
+
+mongod --port $MONGO_PORT --dbpath $LUANDUN_HOME/data/mongodb 2>&1 | cronolog $LUANDUN_HOME/log/mongodb/mongodb.%Y%m%d.log &
