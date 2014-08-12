c591ff310e16e3287b299b50bbe2d639cbc1d95b
prstcsnpr <prstcsnpr@10.2.202.25>
2014-08-12 10:37:43 +0800

    设置port

--- bin/start_beanstalkd.sh
+++ bin/start_beanstalkd.sh
@@ -2,7 +2,11 @@
 
 . env.sh
 
+if [ "x$BEANSTALKD_POST" = "x" ]; then
+	BEANSTALKD_POST = 11300
+fi
+
 mkdir -p $LUANDUN_HOME/data/beanstalkd
 mkdir -p $LUANDUN_HOME/log/beanstalkd
 
-nohup beanstalkd -l 127.0.0.1 -p 11300 -b $LUANDUN_HOME/data/beanstalkd -f0 2>&1 | cronolog $LUANDUN_HOME/log/beanstalkd/beanstalkd.%Y%m%d.log &
+nohup beanstalkd -l 127.0.0.1 -p $BEANSTALKD_POST -b $LUANDUN_HOME/data/beanstalkd -f0 2>&1 | cronolog $LUANDUN_HOME/log/beanstalkd/beanstalkd.%Y%m%d.log &
