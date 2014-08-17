f66fddfbfd1a0b19e74bf99891a36f903e400a1c
prstcsnpr <prstcsnpr@192.168.1.103>
2014-08-17 17:10:47 +0800

    更新category

---
+++ bin/business/magicformula/update_category.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+. env.sh
+
+cd $LUANDUN_HOME/src/luandun/tools/ && python category.py $LUANDUN_HOME/bin/business/magicformula/category
--- src/luandun/tools/category.py
+++ src/luandun/tools/category.py
@@ -8,11 +8,17 @@ Created on 2014年8月16日
 '''
 
 
+import sys
+
 import asyncmongo
 import tornado
 
 
 lines = []
+db = asyncmongo.Client(pool_id="magicformula", 
+                       dbname="magicformula",
+                       host="127.0.0.1",
+                       port=27017)
 
 
 def db_callback(response, error):
@@ -24,18 +30,18 @@ def db_callback(response, error):
                               callback=db_callback)
     else:
         tornado.ioloop.IOLoop.instance().stop()
+        
 
-
-if __name__ == '__main__':
-    db = asyncmongo.Client(pool_id="magicformula", 
-                           dbname="magicformula",
-                           host="127.0.0.1",
-                           port=27017)
-    lines = []
-    with open('/Users/prstcsnpr/Desktop/c') as f:
+def main(argv):
+    with open(argv[1]) as f:
             for line in f.readlines():
                 fields = line.split()
                 lines.append(fields)
     db_callback(None, None)
     tornado.ioloop.IOLoop.instance().start()
+
+
+if __name__ == '__main__':
+    main(sys.argv)
+    
                 
